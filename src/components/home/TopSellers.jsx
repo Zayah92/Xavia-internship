3e0d3706da93a6691c606d0c2a35ae8aa9730c78
Xavia Christopher <xaviachristopher@Xavias-MacBook-Air.local>
2026-04-06 18:40:58 -0400

    Added Top Sellers Dynamic

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,35 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+const [sellers, setSellers] = useState([]);
+const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  axios
+    .get("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers")
+    .then((response) => {
+      setTimeout(() => {
+        setSellers(response.data);
+        setLoading(false);
+      }, 2000); // 👈 keep for testing
+    })
+    .catch((error) => {
+      console.error(error);
+      setLoading(false);
+    });
+}, []);
+
+
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="text-center">
-              <h2>Top Sellers</h2>
+             <h2>Top Sellers</h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {loading
+                ? new Array(12).fill(0).map((_, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <div className="skeleton-circle"></div>
+                      </div>
+
+                      <div className="author_list_info">
+                        <div className="skeleton-text"></div>
+                        <div className="skeleton-text small"></div>
+                      </div>
+                    </li>
+                  ))
+                : sellers.slice(0, 12).map((seller, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
                     <Link to="/author">
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={seller.authorImage}
                         alt=""
-                      />
+                      /> 
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                   <Link to={`/author/${seller.authorId}`}>
+                      {seller.authorName}
+                    </Link>
+                    <span>{seller.price} ETH</span>
                   </div>
                 </li>
               ))}
